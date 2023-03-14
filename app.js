5ff41bddfb05c03d1534ac4572c143a62f000465
NhungTranPY <nhungtranpy91@gmail.com>
2023-03-14 20:32:09 +0700

    485. Sending Responses (HTML Data)

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-    response.write('Hello there')
+    response.setHeader('Content-Type', 'text/html')
+    response.write('<H1>Hello there</H1>')
     response.end()
 })
 server.listen(3000)
