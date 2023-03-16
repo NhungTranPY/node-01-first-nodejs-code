d0cff31a61fe3cae48e1e0b137c39271d46eb0cd
NhungTranPY <nhungtranpy91@gmail.com>
2023-03-16 13:11:14 +0700

    492. Adding basic REST routes

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const locationRoutes = require('./routes/location')
 
 const app = express()
 
-app.set('view engine', 'ejs')
-app.set('views', 'views') // #490
+// app.set('view engine', 'ejs')
+// app.set('views', 'views') // #490
 
-app.use(bodyParser.urlencoded({extended: false})) // parsing the request body
+// app.use(bodyParser.urlencoded({extended: false})) // parsing the request body
 
-app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'text/html')
-    next()
-})
+// app.use((req, res, next) => {
+//     res.setHeader('Content-Type', 'text/html')
+//     next()
+// })
 
-app.use((req, res, next) => {
-    const userName = req.body.username || 'Unknown user'
-    res.render('index', {
-        user: userName
-    })
-})
+// app.use((req, res, next) => {
+//     const userName = req.body.username || 'Unknown user'
+//     res.render('index', {
+//         user: userName
+//     })
+// })
+
+app.use(bodyParser.json())
+app.use(locationRoutes)
 
 app.listen(3000)
